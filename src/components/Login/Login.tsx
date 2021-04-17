fbefc52ef461cf3e7a889d5e3e43982f6362415c
SDen4 <asw325@yandex.ru>
2021-04-17 20:42:49 +0500

    authorisation logic in firebase

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,14 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 import styles from './Login.module.css';
 
 const Login: React.FC = () => {
-  const registrHandler = () => {
-    console.log('registrHandler');
+  const [emailValue, setEmailValue] = useState('');
+  const [passwordValue, setPasswordValue] = useState('');
+
+  const emailChangeHandler = (event: any) => {
+    setEmailValue(event.target.value);
+    console.log(emailValue);
   };
-  const loginHandler = () => {
-    console.log('loginHandler');
+
+  const passwordChangeHandler = (event: any) => {
+    event.preventDefault();
+    setPasswordValue(event.target.value);
+    console.log(passwordValue);
   };
+
+  const registrHandler = async () => {
+    const authData = {
+      email: emailValue,
+      password: passwordValue,
+      returnSecureToken: true,
+    };
+    try {
+      const response = await axios.post(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyD7qlRU6UjFDCiXBWJkGPxDbfkvGv8MauI',
+        authData,
+      );
+      console.log(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const loginHandler = async () => {
+    const authData = {
+      email: emailValue,
+      password: passwordValue,
+      returnSecureToken: true,
+    };
+    try {
+      const response = await axios.post(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyD7qlRU6UjFDCiXBWJkGPxDbfkvGv8MauI',
+        authData,
+      );
+      console.log(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   const submitHandler = (event: React.ChangeEvent<HTMLFormElement>) => {
     event.preventDefault();
     console.log('submitHandler');
@@ -18,14 +61,25 @@ const Login: React.FC = () => {
     <div className={styles.component}>
       <div className={styles.window}>
         <h2>Authorisation</h2>
+
         <form className={styles.form} onSubmit={submitHandler}>
           <div className={styles.formUnit}>
             <label htmlFor="email">E-mail</label>
-            <input type="email" id="email" />
+            <input
+              type="email"
+              id="email"
+              value={emailValue}
+              onChange={emailChangeHandler}
+            />
           </div>
           <div className={styles.formUnit}>
             <label htmlFor="password">Password</label>
-            <input type="password" id="password" />
+            <input
+              type="password"
+              id="password"
+              value={passwordValue}
+              onChange={passwordChangeHandler}
+            />
           </div>
           <div className={styles.btnsWrapper}>
             <button type="button" onClick={registrHandler}>
