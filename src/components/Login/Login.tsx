b3830691ee428fad8f69d6ed35ccb768f6ff8d90
SDen4 <asw325@yandex.ru>
2021-04-17 20:05:32 +0500

    add axios

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -3,21 +3,37 @@ import React from 'react';
 import styles from './Login.module.css';
 
 const Login: React.FC = () => {
+  const registrHandler = () => {
+    console.log('registrHandler');
+  };
+  const loginHandler = () => {
+    console.log('loginHandler');
+  };
+  const submitHandler = (event: React.ChangeEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log('submitHandler');
+  };
+
   return (
     <div className={styles.component}>
       <div className={styles.window}>
-        <form className={styles.form}>
+        <h2>Authorisation</h2>
+        <form className={styles.form} onSubmit={submitHandler}>
           <div className={styles.formUnit}>
-            <label>E-mail</label>
-            <input type="email" />
+            <label htmlFor="email">E-mail</label>
+            <input type="email" id="email" />
           </div>
           <div className={styles.formUnit}>
-            <label>Password</label>
-            <input type="password" />
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" />
           </div>
           <div className={styles.btnsWrapper}>
-            <button type="button">Registration</button>
-            <button type="submit">Enter</button>
+            <button type="button" onClick={registrHandler}>
+              Registration
+            </button>
+            <button type="submit" onClick={loginHandler}>
+              Enter
+            </button>
           </div>
         </form>
       </div>
